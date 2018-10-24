2521ba321ec522b52f396a132aeb41fe2aef7652
jlgarcia90 <44421581+jlgarcia90@users.noreply.github.com>
2018-10-23 21:19:15 -0700

    Add files via upload

---
+++ receipt.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title> Receipt </title>
+</head>
+<body>
+
+<fieldset id="response">
+
+    <div>
+        form id="Receipt"
+            /*
+             php code to essentially call back fields from the previous two pages then print them out the end user
+             */
+    </div>
